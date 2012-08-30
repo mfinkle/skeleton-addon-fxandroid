23b9798352bef2cb42193610d9327e0003e44466
Margaret Leibovic <margaret.leibovic@gmail.com>
2012-08-30 16:36:25 -0700

    Revert "view source add-on"
    This reverts commit 8a502f8294e34c5dfc88566536309d13162ea7eb.

--- bootstrap.js
+++ bootstrap.js
@@ -4,33 +4,63 @@ const Cu = Components.utils;
 
 Cu.import("resource://gre/modules/Services.jsm");
 
-function log(message) {
-  Services.console.logStringMessage(message);
+function isNativeUI() {
+  let appInfo = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
+  return (appInfo.ID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}");
 }
 
-var menuId = null;
+function showToast(aWindow) {
+  aWindow.NativeWindow.toast.show("Showing you a toast", "short");
+}
+
+function showDoorhanger(aWindow) {
+  buttons = [
+    {
+      label: "Button 1",
+      callback: function() {
+        aWindow.NativeWindow.toast.show("Button 1 was tapped", "short");
+      }
+    } , {
+      label: "Button 2",
+      callback: function() {
+        aWindow.NativeWindow.toast.show("Button 2 was tapped", "short");
+      }
+    }];
+
+  aWindow.NativeWindow.doorhanger.show("Showing a doorhanger with two button choices.", "doorhanger-test", buttons);
+}
+
+function copyLink(aWindow, aTarget) {
+  let url = aWindow.NativeWindow.contextmenus._getLinkURL(aTarget);
+  aWindow.NativeWindow.toast.show("Todo: copy > " + url, "short");
+}
+
+var gToastMenuId = null;
+var gDoorhangerMenuId = null;
+var gContextMenuId = null;
 
 function loadIntoWindow(window) {
-  log("loadIntoWindow");
   if (!window)
     return;
 
-  menuId = window.NativeWindow.menu.add("View Source", null, function() {
-    viewSource(window);
-  });
+  if (isNativeUI()) {
+    gToastMenuId = window.NativeWindow.menu.add("Show Toast", null, function() { showToast(window); });
+    gDoorhangerMenuId = window.NativeWindow.menu.add("Show Doorhanger", null, function() { showDoorhanger(window); });
+    gContextMenuId = window.NativeWindow.contextmenus.add("Copy Link", window.NativeWindow.contextmenus.linkOpenableContext, function(aTarget) { copyLink(window, aTarget); });
+  }
 }
 
 function unloadFromWindow(window) {
   if (!window)
     return;
 
-  window.NativeWindow.menu.remove(menuId);
+  if (isNativeUI()) {
+    window.NativeWindow.menu.remove(gToastMenuId);
+    window.NativeWindow.menu.remove(gDoorhangerMenuId);
+    window.NativeWindow.contextmenus.remove(gContextMenuId);
+  }
 }
 
-function viewSource(window) {
-  window.console.log("view-source@mydomain.org: displaying source for " + window.content.location.href);
-  window.BrowserApp.addTab("view-source:" + window.content.location.href);
-}
 
 /**
  * bootstrap.js API
